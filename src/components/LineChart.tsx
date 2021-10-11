fdf6d5b5b6629bd4994f8649ffac00ef988047cf
Dmitry <pro100pro10010@gmail.com>
2021-10-11 18:30:43 +0300

    Add [skipping lables, input field]

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -45,7 +45,7 @@ Chart.register(zoomPlugin);
 // };
 const options: any = {
     // radius: 0,
-    bezierCurve : false,
+    bezierCurve: false,
     interaction: {
         intersect: false
     },
@@ -109,6 +109,45 @@ function genDatasets(data: any) {
 //         spanGaps: true
 //     }]
 // };
+const labels: any = [1, 2, 3, 4, 5];
+const dataB: any = {
+    datasets: [{
+        label: "f(x) = x^2",
+        function: function (x: any) { return x * x },
+        borderColor: "rgba(75, 192, 192, 1)",
+        data: [{ x: "1", y: 1 }, { x: "2", y: 4 }, { x: "3", y: 9 }, { x: "4", y: 16 }, { x: "5", y: 25 }],
+        fill: false,
+        pointRadius: 0,
+    },
+    {
+        label: "f(x) = x² round",
+        function: function (x: number) { return x * x },
+        borderColor: "rgba(153, 102, 255, 1)",
+        data: [{ x: "1", y: 1 }, { x: "3", y: 9 }, { x: "5", y: 25 }],
+        fill: false,
+        spanGaps: true
+    }]
+};
+
+const arr: any = [1, 3, 5];
+const optionsB: any = {
+    // radius: 0,
+    parsing: {
+        yAxisKey: 'y',
+        xAxisKey: 'x'
+    },
+    scales: {
+        xAxis: {
+            ticks: {
+                callback: function (value: any, index: any, values: any) {
+                    const v = labels[value];
+                    if (arr.indexOf(v) !== -1) return v;
+                    return '';
+                }
+            }
+        }
+    }
+};
 
 
 const ChartWrapper = styled.div`
@@ -116,7 +155,7 @@ const ChartWrapper = styled.div`
     margin: 0 auto;
 `;
 
-const LineChart : React.FunctionComponent = () => {
+const LineChart: React.FunctionComponent = () => {
 
 
     const [data, setData] = useState(genDatasets(genData(10)));
@@ -124,11 +163,26 @@ const LineChart : React.FunctionComponent = () => {
 
     const [parentVal, setParentVal] = useState(10);
     const sliderValueChanged = (val: any) => {
-        console.log("NEW VALUE", val);
+        // console.log("NEW VALUE", val);
         setData(genDatasets(genData(val)));
-        console.log(data)
+        // console.log(data)
         setParentVal(val);
     };
+
+
+    const [getN, setN] = useState(50);
+
+    const [inputN, setInputN] = useState(50);
+    const inputsHandler = (e: any) => {
+        console.log(e.target.value)
+        setInputN(e.target.value);
+        // console.log(inputN);
+        setN(parseInt(e.target.value));
+        sliderValueChanged(e.target.value);
+    }
+
+
+
 
     return (
         <>
@@ -138,13 +192,18 @@ const LineChart : React.FunctionComponent = () => {
             <div>
                 {/* <h1>PARENT VALUE: {parentVal}</h1> */}
                 <RangeSlider classes="additional-css-classes"
-                label={"N"}
-                onChange={(e: any) => sliderValueChanged(e)}
-                value={parentVal}
-                min={2}
-                max={50}
-                step={1} />
+                    label={"N"}
+                    onChange={(e: any) => sliderValueChanged(e)}
+                    value={parentVal}
+                    min={2}
+                    max={getN}
+                    step={1} />
             </div>
+            <input
+                type="number"
+                name="n"
+                onChange={inputsHandler}
+                value={inputN} />
         </>
     );
 };
