2d277d1d14c960c24b3ccf8f74c553ff168d9ff8
Dmitry <pro100pro10010@gmail.com>
2021-10-13 00:03:30 +0300

    Add [formula info] global

--- src/components/GlobalInfo.tsx
+++ src/components/GlobalInfo.tsx
@@ -4,11 +4,20 @@ const Wrapper = styled.div`
     text-align: center;
 `;
 
+const FormulaWrapper = styled.h2`
+    text-align: center;
+    border: black 2px solid;
+    padding: 7px;
+    width: 12rem;
+    margin: 5px auto;
+`;
+
 const GlobalInfo: React.FunctionComponent = () => {
     return <Wrapper>
         <h1>Computational Practicum for DE Course </h1>
         <h3>powered by dsomni (d.beresnev@innopolis.university), IU B20-02</h3>
         <hr />
+        <FormulaWrapper>y' = y/x - y - x</FormulaWrapper>
     </Wrapper>
 };
 
